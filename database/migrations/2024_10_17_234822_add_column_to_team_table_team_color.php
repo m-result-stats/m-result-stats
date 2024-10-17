e6e8fc698340bf9bfef71f9bc9a8457ec76f6a8a
scudelia <sabres@ac.auone-net.jp>
2024-10-17 16:09:18 +0000

    チームカラーの項目を追加

---
+++ database/migrations/2024_10_17_234822_add_column_to_team_table_team_color.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('team_color', length: 10)->after('team_name')->comment('チームカラー');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropColumn('team_color');
+        });
+    }
+};
--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -16,15 +16,15 @@ class TeamSeeder extends Seeder
         Team::truncate();
         //
         $data = [
-            ['team_id' => 1, 'team_name' => '赤坂ドリブンズ',],
-            ['team_id' => 2, 'team_name' => 'EX風林火山',],
-            ['team_id' => 3, 'team_name' => 'KADOKAWAサクラナイツ',],
-            ['team_id' => 4, 'team_name' => 'KOMAMI麻雀格闘倶楽部',],
-            ['team_id' => 5, 'team_name' => '渋谷ABEMAS',],
-            ['team_id' => 6, 'team_name' => 'セガサミーフェニックス',],
-            ['team_id' => 7, 'team_name' => 'TEAM雷電',],
-            ['team_id' => 8, 'team_name' => 'BEASTX',],
-            ['team_id' => 9, 'team_name' => 'U-NEXT Pirates',],
+            ['team_id' => 1, 'team_name' => '赤坂ドリブンズ', 'team_color' => '387d39',],
+            ['team_id' => 2, 'team_name' => 'EX風林火山', 'team_color' => 'ae0000',],
+            ['team_id' => 3, 'team_name' => 'KADOKAWAサクラナイツ', 'team_color' => 'c91e7f',],
+            ['team_id' => 4, 'team_name' => 'KOMAMI麻雀格闘倶楽部', 'team_color' => 'ea0005',],
+            ['team_id' => 5, 'team_name' => '渋谷ABEMAS', 'team_color' => 'a86400',],
+            ['team_id' => 6, 'team_name' => 'セガサミーフェニックス', 'team_color' => 'e14909',],
+            ['team_id' => 7, 'team_name' => 'TEAM雷電', 'team_color' => 'dd6700',],
+            ['team_id' => 8, 'team_name' => 'BEASTX', 'team_color' => '004d25',],
+            ['team_id' => 9, 'team_name' => 'U-NEXT Pirates', 'team_color' => '0e0280',],
         ];
         Team::insert($data);
     }
