3c0ba8d20e05a3f0e76eaa7620c805a4c46806de
scudelia <sabres@ac.auone-net.jp>
2024-11-03 01:31:08 +0000

    共通関数トレイトに曜日を取得する関数を追加

--- app/Traits/CommonFunctionsTrait.php
+++ app/Traits/CommonFunctionsTrait.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use DateTime;
 use Illuminate\Http\Request;
 
 /**
@@ -22,5 +23,29 @@ trait CommonFunctionsTrait
                 $key => $value,
             ]);
         }
+    }
+
+    /**
+     * 日付から曜日を取得する
+     *
+     * @param DateTime $date 日付
+     * @return string 曜日
+     */
+    public function getWeekName(DateTime $date): string
+    {
+        $weeks = ['日','月','火','水','木','金','土',];
+        return $weeks[$date->format('w')];
+    }
+
+    /**
+     * 日付と曜日を同時に表示する
+     *
+     * @param DateTime $date 日付
+     * @return string "yyyy-mm-dd(曜日)"の形式
+     */
+    public function getWithWeekName(DateTime $date): string
+    {
+        $weekName = $this->getWeekName($date);
+        return "{$date->format('Y-m-d')}({$weekName})";
     }
 }
