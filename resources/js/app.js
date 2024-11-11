be094746bc469dadbb126d26cf03c555edd6fa54
scudelia <sabres@ac.auone-net.jp>
2024-11-11 13:36:40 +0000

    ツールチップを有効にする

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,12 @@
 import './bootstrap';
 import './team-point-chart.js';
+
+import * as bootstrap from 'bootstrap';
+
+// ページロード後にツールチップを初期化
+document.addEventListener('DOMContentLoaded', () => {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
