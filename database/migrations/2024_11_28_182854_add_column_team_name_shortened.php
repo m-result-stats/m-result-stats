5879406e5e1a76a74c0c3f537876a51c04d77e80
scudelia <sabres@ac.auone-net.jp>
2024-11-28 09:42:23 +0000

    チーム名の短縮表記名を追加

---
+++ database/migrations/2024_11_28_182854_add_column_team_name_shortened.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('team_name_shortened', 20)->after('team_name')->comment('チーム名短縮');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropColumn([
+                'team_name_shortened',
+            ]);
+        });
+    }
+};
