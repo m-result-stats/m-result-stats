ef2350aae9439143b198218db9abf48573c176c4
scudelia <sabres@ac.auone-net.jp>
2024-12-05 12:51:35 +0000

    チームモデルの改良
    - 持ち越しポイントテーブルとの結合の追加
    - チームIDでの絞り込みの追加

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Kyslik\ColumnSortable\Sortable;
 
@@ -14,4 +16,20 @@ class Team extends Model
     use Sortable;
     // 主キー
     protected $primaryKey = 'team_id';
+
+    /**
+     * 持ち越しポイントポイントテーブルとの結合
+     */
+    public function carriedOverPoint(): HasOne
+    {
+        return $this->hasOne(CarriedOverPoint::class, 'team_id', 'team_id');
+    }
+
+    /**
+     * チームでの絞り込み
+     */
+    public function scopeEqualTeamId(Builder $query, int $value): void
+    {
+        $query->where('team_id', $value);
+    }
 }
